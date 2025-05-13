ac7d6cf155d61460929c175bb5af33af065922de
Naish Abbas <naish031@gmail.com>
2025-05-14 01:26:28 +0500

    fix: update content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,13 +12,15 @@ export default function Home() {
       <span className="text-4xl">👋</span>
       <Heading className="font-black">Hello there! I&apos;m Naish</Heading>
       <Paragraph className="max-w-xl mt-4">
-        I&apos;m a frontend developer who loves{" "}
-        <Highlight>crafting beautiful, interactive interfaces</Highlight> that engage users at every click
+        I&apso;m a frontend web developer with{" "}
+        <Highlight>2 years of hands-on experience</Highlight> building
+        responsive, high-performance websites that blend design and
+        functionality. I specialize in{" "}
+        <Highlight>crafting beautiful, interactive user interfaces</Highlight>{" "}
+        that not only look great but also deliver seamless user experiences
+        across all devices.
       </Paragraph>
-      <Paragraph className="max-w-xl mt-4">
-        I&apos;m a software engineer with{" "}
-        <Highlight>2 years of experience</Highlight> building portfolio websites and interactive designs used by a larger audience.
-      </Paragraph>
+
       <Heading
         as="h2"
         className="font-black text-lg md:text-lg lg:text-lg mt-20 mb-4"
